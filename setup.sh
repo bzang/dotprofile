83de58139085c2e07d3dcaa7b896c0c690fe4c77
bzang <bzang@50onred.com>
2014-09-14 12:32:55 -0400

    Added initial package install and setup?

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check if ruby is available
+
+required_commands = ('ruby');
+
+for i in $( required_commands ); do
+	command -v $i >/dev/null 2>&1 || { echo >&2 "$i is required."; exit 1; }
+done
+
+# Check for and install command line tools
+if [ $(pkgutil --pkg-info=com.apple.pkg.CLTools_Executables) !=~ 'package-id' ]; then
+	xcode-select --install
+fi
+
+# install homebrew
+if ! command -v "brew" > /dev/null; then
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# install a bunch of brew stuff
+brew install git libmagic imagemagick ruby redis mysql node python tig
+
+# install global python plugins
+if command -v "pip" > /dev/null; then
+	pip install -r virtualenv virtualenvwrapper flake8 cssmin
+else
+	echo "npm is required.";
+fi
+
+# install global node packages
+if command -v "npm" > /dev/null; then
+	npm update
+	npm install -g grunt-cli less uglify-js
+else
+	 echo "npm is required.";
+fi
